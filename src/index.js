562da1154929238c103396245d55be4bfe890ef7
Edward Roberts <Edward_Roberts@epam.com>
2018-11-01 14:31:40 +0100

    Latest changes from course

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {getUsers} from './api/userApi';
+import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
@@ -15,4 +16,18 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must user array.from to create a real array from a DOM collection
+    // getElementByClassName only returns an 'array like' object
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
