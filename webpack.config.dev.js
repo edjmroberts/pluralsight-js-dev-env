c2e62cf6a353658dd6afd3eaa31de61d1ff0e654
Edward Roberts <Edward_Roberts@epam.com>
2019-06-16 20:11:25 +0200

    Needed to upgrade to newer babel, and security check has changed to npm audit in npm version 6 In addition faker stuff needs to be added as an extension

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,11 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    debug: true,
+    mode: 'development',
     devtool: 'inline-source-map',
-    noInfo: false,
+    devServer: {
+        noInfo: false,
+    },
     entry: [
         path.resolve(__dirname, 'src/index')
     ],
@@ -22,9 +24,19 @@ export default {
         })
     ],
     module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/, loaders: ['style','css']}
-        ]
+        rules: [{
+            test: /\.js$/,
+            use: [{
+                loader: "babel-loader"
+            }]
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }]
+        }]
     }
 }
